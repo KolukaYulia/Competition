89b4b6ccd0f2c774fb175ad2ed2e6e65ca1a9b38
“Koluka” <koluka78@mail.ru>
2023-03-30 19:44:27 +0300

    AddClasses

---
+++ src/main/java/NotRegisteredException.java
@@ -0,0 +1,6 @@
+public class NotRegisteredException extends RuntimeException {
+    public NotRegisteredException(String playerName) {
+        super("Игрок" + playerName + "не найден");
+    }
+
+}
